cef8e40ea8106f356a9ad84abbaaadaa60d931c8
ARuizma <andresrumarin@gmail.com>
2023-07-11 17:35:27 +0200

    Finished PredicateJoins

--- src/main/java/java8features/predicate/PredictateJoins.java
+++ src/main/java/java8features/predicate/PredictateJoins.java
@@ -1,10 +1,24 @@
 package java8features.predicate;
-
 import java.util.function.Predicate;
 
 public class PredictateJoins {
 
-    int [] x = {0,10,20,30,40,50,60,70,73};
+    public static void main(String[] args) {
 
-    Predicate<Integer> p1 = i ->
+
+        int[] x = {0, 10, 20, 30, 40, 50, 60, 70, 73};
+
+        Predicate<Integer> p1 = i -> i > 10;
+        System.out.println("Greater thatn 10:");
+        method1(p1,x);
+    }
+
+    static void method1(Predicate<Integer>p, int[] x){
+        for (int eachValue : x) {
+            if(p.test(eachValue)){
+                System.out.println(eachValue);
+            }
+            
+        }
+    }
 }
